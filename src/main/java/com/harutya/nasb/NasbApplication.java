9b39136f6b3963e59c685070150c61fbcab5cb85
unknown <thouartmyhope@foxmail.com>
2021-03-24 00:13:01 +0800

    init
    Init the project.

---
+++ src/main/java/com/harutya/nasb/NasbApplication.java
@@ -0,0 +1,13 @@
+package com.harutya.nasb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NasbApplication.class, args);
+    }
+
+}
